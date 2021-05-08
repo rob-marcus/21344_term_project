361ef7bb05427c6a017764dbbd5ec4073c937756
Rob Marcus <robmarcus@google.com>
2021-05-07 23:37:14 -0400

    Input validation done (untested.)

---
+++ src/constants.py
@@ -0,0 +1,128 @@
+"""A collection of constants used throughout the program.
+
+@author: <rbm@cmu.edu>
+@date: 05/07/21
+"""
+import os
+import glob
+import time
+
+# Set of valid image extensions to process.
+IMG_EXTS = {".jpg", 
+            ".jpeg", 
+            ".tif", 
+            ".tiff",
+            ".png"}
+
+def validate_in_path(path, param_str): 
+  """Verifies that a path leads to either an image, or a non-empty image 
+  folder.
+  
+  Args:
+    path (str): path to either an existing image or a folder with at 
+      least one image.
+    param_str (str): the parameter being checked, e.g., "virtual".
+  Returns: 
+    (NoneType): None if the predicate holds. Otherwise will raise an 
+      assertion error.  
+  """
+  # Check if is a file. 
+  if os.path.isfile(path): 
+    ext = os.path.splitext(path)[-1].lower()
+    if ext not in IMG_EXTS: 
+      err_str = ("Path error for {} argument: the file at path {} " + \
+                 "is not a recognized image type. " + \
+                 "Recognized extensions are {}."
+                ).format(param_str, path, IMG_EXTS)
+
+      raise AssertionError(err_str)
+  elif os.path.isdir(path):
+    contains_valid_ext = False
+
+    for ext in IMG_EXTS:
+      if glob.glob("*" + ext) != []:
+        contains_valid_ext = True
+
+    if not contains_valid_ext:
+      err_str = ("Path error for {} argument: the folder {} has " + \
+                 "no files with valid image extensions. " + \
+                 "Recognized extensions are {}."
+                ).format(param_str, path, IMG_EXTS)
+
+      raise AssertionError(err_str)
+  else: # path leads to nothing.
+    raise AssertionError("The path {} does not exist".format(path))
+
+  return None
+
+def validate_out_path(path): 
+  """Checks if an output path to a directory exists, otherwise creates the dir.
+
+  Args:
+    path (str): path to either an existing image or a folder with at 
+      least one image.
+  Returns: 
+    (NoneType): None.
+  """
+  if not os.path.isdir(path):
+    os.mkdir(path)
+
+  return None
+
+class Params():
+  """Class for parameters (arguments) used to compile the program. 
+
+  """
+
+  def __init__(self, virtual, color, blur, time, debug, out_path, in_path, stitch):
+    self.virtual = virtual
+    self.color = color
+    self.blur = blur
+    self.time = time
+    self.debug = debug
+    self.out_path = out_path
+    self.in_path = in_path
+    self.stitch = stitch
+
+    self.virtual_str = "virtual"
+    self.color_str = "color"
+    self.blur_str = "blur"
+    self.time = "time"
+    self.in_path_str = "in_path"
+
+    self.validate_inputs()
+
+  def validate_inputs(self): 
+    """Verifies that each input is well-formed.
+
+    Returns:
+      (NoneType): None if all inputs are well-formed. Otherwise will raise an 
+        assertion error for the first malformed input found. 
+    """
+    if self.virtual:
+      validate_in_path(self.virtual, self.virtual_str)
+
+    if self.color: 
+      assert(self.color in {"white", "blue", "yellow", "random"})
+
+    if self.blur:
+      assert(self.blur % 2 == 1 and self.blur > 0)
+
+    # Make sure we are only specifying one background type. 
+    if (self.virtual and self.color) or (self.virtual and self.blur) or \
+      (self.color and self.blur): 
+      raise AssertionError("Please specify only one background type.")
+    if self.out_path: 
+      validate_out_path(self.out_path)
+    else: # make the outpath just the current systime. 
+      out_path = str(time.time())[:10] + str(time.time())[11:]
+      self.out_path = out_path
+      os.mkdir(out_path)
+      print("Out path was unspecified.")
+      print(("Processed images will be stored at {} "
+            + "in the program folder").format(self.out_path))
+
+    if self.in_path: # else we read from webcam. 
+      validate_in_path(self.in_path, self.in_path_str)
+
+    return None
